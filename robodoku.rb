d2a877ba22467fda0c01bd9be8230329551e1c03
Patrice Cadiot <pathibul.r@gmail.com>
2016-05-27 21:53:00 +0200

    ajout version avec (la) classe

--- robodoku.rb
+++ robodoku.rb
@@ -24,84 +24,77 @@
 # def test_square(carre)
 
 # end
-class Solver
-	def initialize(puzzle)
-		@puzzle = puzzle
-	end
 
-	def transformation_en_matrice(puzzle)
-		@tab = Array.new(9) { Array.new(9) { 0 } }
-		@puzzle.each do |car|
-			if car == '\n' or car == '\r\n'
-				i += 1 
-			elsif car == " "
-				@tab[i][j] = 0
-			else
-				@tab[i][j] = car.to_i
+def transformation_en_matrice(puzzle)
+	@tab = Array.new(9) { Array.new(9) { 0 } }
+	@puzzle.each do |car|
+		if car == '\n' or car == '\r\n'
+			i += 1 
+		elsif car == " "
+			@tab[i][j] = 0
+		else
+			@tab[i][j] = car.to_i
+		end
+		j += 1
+	end
+end
+
+def charge(item)
+	item.inject(0) do |compteur, nb|
+		nb != 0 ? compteur += 1 : compteur
 			end
-			j += 1
-		end
+end
+
+
+def test_unicite(item)
+	# item.inject(Hash.new(0)) do |accu, cell|
+	# 	accu[cell] += 1
+	# 	return false if accu[cell] > 1
+	# end
+	@res = Hash.new(0)
+	item.each do |nombre|
+		@res["#{nombre}"] += 1
+		return false if @res["#{nombre}"] > 1
 	end
-	
-	def charge(item)
-  	item.inject(0) do |compteur, nb|
-  		nb != 0 ? compteur += 1 : compteur
- 			end
-	end
-
-
-	def test_unicite(item)
-		# item.inject(Hash.new(0)) do |accu, cell|
-		# 	accu[cell] += 1
-		# 	return false if accu[cell] > 1
-		# end
-		@res = Hash.new(0)
-		item.each do |nombre|
-			@res["#{nombre}"] += 1
-			return false if @res["#{nombre}"] > 1
-		end
-
-	end
-
-	def ligne(abs, ord)
-		@tab[abs]
-	end
-
-	def colonne(abs, ord)
-		@tab.map{|cell| cell[ord]}
-	end
-
-	def carre(abs, ord)
-		abs_carr = abs / 3
-		ordo_carr = ord / 3
-		@carre = Array.new(3) { Array.new(3) { 0 } }
-		# @tab.each do |x|
-		# 	x.each do |y|
-		# 		@carre << y
-		# 	end
-		# end
-		# @carre
-		# @carre << @tab[abs_carr..(abs_carr + 2)][ordo_carr..(ordo_carr..ordo_carr+2)]
-		# @tab.map do |x| 
-		# 	if abs_carr
-		# 		x.map do |y| 
-		# 		y 
-		# 	end
-		# end
-		indx = 0
-		@tab.each_with_index do |e, i|
-			if ( i >= abs_carr or i <= abs_carr + 2 )
-				e.each_with_index do |el, j|
-					@carre[indx] << el if ( j >= ordo_carr or j <= ordo_carr + 2  )
-				end
-				indx += 1
-			end
-		end
-	end
-	def solver
-
-	end
-
-
 
 end
+
+def ligne(abs, ord)
+	@tab[abs]
+end
+
+def colonne(abs, ord)
+	@tab.map{|cell| cell[ord]}
+end
+
+def carre(abs, ord)
+	abs_carr = abs / 3
+	ordo_carr = ord / 3
+	@carre = Array.new(3) { Array.new(3) { 0 } }
+	# @tab.each do |x|
+	# 	x.each do |y|
+	# 		@carre << y
+	# 	end
+	# end
+	# @carre
+	# @carre << @tab[abs_carr..(abs_carr + 2)][ordo_carr..(ordo_carr..ordo_carr+2)]
+	# @tab.map do |x| 
+	# 	if abs_carr
+	# 		x.map do |y| 
+	# 		y 
+	# 	end
+	# end
+	indx = 0
+	@tab.each_with_index do |e, i|
+		if ( i >= abs_carr or i <= abs_carr + 2 )
+			e.each_with_index do |el, j|
+				@carre[indx] << el if ( j >= ordo_carr or j <= ordo_carr + 2  )
+			end
+			indx += 1
+		end
+	end
+end
+def solver
+
+end
+
